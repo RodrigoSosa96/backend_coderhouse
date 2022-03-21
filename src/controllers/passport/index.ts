074d65779f5b3a93c715039ec13591fa39c75162
Rodrigo <rod2008@live.com.ar>
2022-03-21 20:30:31 -0300

    Controlador agregado a passport

--- src/controllers/passport/index.ts
+++ src/controllers/passport/index.ts
@@ -1,65 +1,15 @@
 import { unlink } from "fs/promises";
-import mongoose from "mongoose";
 import { PassportStatic } from "passport";
 import { Strategy } from "passport-local";
 import { UserModel, User } from "../../models";
-import Logger from "../../utils/logger";
+import UserController from "../user/User.controller";
 
 export function passport_load(passport: PassportStatic) {
-    passport.use("signup", new Strategy({ passReqToCallback: true, usernameField: "email" },
-        async (req, email, password, done) => {
-            try {
-                const body = req.body as User
-                const newUser = await UserModel.create({
-                    email,
-                    password,
-                    name: body.name,
-                    address: body.address,
-                    age: body.age,
-                    phoneNumber: body.phoneNumber,
-                    picture: req.file?.filename,
-                    role: body.role
-                })
-                const pojo = newUser.toObject()
-                console.log(pojo)
-                return done(null, pojo);
-            } catch ({code}) {
-                if(req.file) {
-                    console.log("Borrando archivo")
-                    await unlink(req.file.path)
-                }
-                if (code === 11000) {
-                    return done(null, false, { message: "Usuario ya existe" });
-                }
-                return done(null, false, { message: "Error validando los datos" });
-            }
-        }
-    ));
-
-
-    passport.use("login", new Strategy({ passReqToCallback: true, usernameField: "email" },
-        async (_req, email: string, password: string, done) => {
-            try {
-                const user = await UserModel.findOne({ email }).exec();
-                if (!user) {
-                    return done(null, false, { message: "Usuario no existe" });
-                }
-                if (!await user.comparePassword(password)) {
-                    return done(null, false, { message: "Contraseña incorrecta" });
-                }
-                return done(null, user.toObject());
-            }
-            catch  {
-                done("Error validando los datos");
-            }
-        }
-    ));
-
-
+    passport.use("signup", new Strategy({ passReqToCallback: true, usernameField: "email" }, UserController.createLocal));
+    passport.use("login", new Strategy({ passReqToCallback: true, usernameField: "email" }, UserController.loginLocal));
     passport.serializeUser((user, done) => {
         done(null, user._id);
     });
-
     passport.deserializeUser(async (id: string, done) => {
         try {
             const user = await UserModel.findById(id).lean().exec();
@@ -70,4 +20,3 @@ export function passport_load(passport: PassportStatic) {
         }
     });
 }
-
---
+++ src/controllers/user/User.controller.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import { unlink } from "fs/promises";
+import { User, UserModel } from "../../models";
+import {IVerifyOptions, VerifyFunctionWithRequest} from "passport-local";
+
+class UserController {
+
+    static async get (req: Request, res: Response, next: NextFunction) {
+        try {
+            const users = await UserModel.find().orFail().exec();
+            return res.status(200).json(users);
+        } catch (err) {
+            next(err);
+        }
+    }
+    static async createLocal (req:Request, email:string, password:string, done: Function) {
+        try {
+            const body = req.body as User
+            const newUser = await UserModel.create({
+                email,
+                password,
+                name: body.name,
+                address: body.address,
+                age: body.age,
+                phoneNumber: body.phoneNumber,
+                picture: req.file?.filename,
+                role: body.role
+            })
+            const pojo = newUser.toObject()
+            console.log(pojo)
+            return done(null, pojo);
+        } catch ({code}) {
+            if(req.file) {
+                console.log("Borrando archivo")
+                await unlink(req.file.path)
+            }
+            if (code === 11000) {
+                return done(null, false, { message: "Usuario ya existe" });
+            }
+            return done(null, false, { message: "Error validando los datos" });
+        }
+    }
+
+    static async loginLocal (_req:Request, email:string, password:string, done:Function) {
+            try {
+                const user = await UserModel.findOne({ email }).exec();
+                if (!user) {
+                    return done(null, false, { message: "Usuario no existe" });
+                }
+                if (!await user.comparePassword(password)) {
+                    return done(null, false, { message: "Contraseña incorrecta" });
+                }
+                return done(null, user.toObject());
+            }
+            catch  {
+                done("Error validando los datos");
+            }
+        
+    }
+}
+
+export default UserController;
